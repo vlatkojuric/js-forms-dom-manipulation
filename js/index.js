fa47d6cd24beb4d026e816419cc7a55df3d0bf0b
Vlatko Juric <juricv78gmail.com>
2023-10-04 16:43:27 +0200

    challenge from the afternoon,form DOM manipulation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // const formElements = event.target.elements;
+
+  // console.log(formElements.firstName.value);
+  // console.log(formElements.lastName.value);
+  // console.log(formElements.age.value);
+  // console.log(formElements.email.value);
+  // console.log(formElements.complaint.value);
+  // console.log(formElements.details.value);
+  // console.log(formElements.badness.value);
+  // console.log(formElements.orderDate.value);
+  // console.log(formElements.tos.value);
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
